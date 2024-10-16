5c6fc796b1fe4c917666fc59eda2006bd50fcd4b
misterPython3 <genesismatic03@gmail.com>
2024-10-16 10:48:36 +0800

    added models, updated controllers, new migrations

--- buildit-pc-backend/app/Http/Controllers/UserController.php
+++ buildit-pc-backend/app/Http/Controllers/UserController.php
@@ -3,33 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Hash;
-use Illuminate\Hashing\BcryptHasher;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Str;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function authenticate(Request $request){
-        $json_body = $request->json()->all();
-        $user = User::where("email", $json_body["email"]);
-        if(!$user->exists())
-            return response("Account does not exist",404);
-
-        if(Hash::check($json_body["password"],$user->first()->password)){
-            return response("OK",200);
-        }
-
-        return response("login failed", 403);
+        return $request->user();
     }
-    public function store(Request $request){
-        $json_body = $request->json()->all();
-        if(User::where("email", $json_body["email"])->exists()){
+    public function login(Request $request){
+        $user = $request->validate([
+            "name" => ["required"],
+            "password" ["required"]
+        ]);
+        $user = User::where('name',$user['name']);
+        if(!$user->exists())
+            return response(['message' => 'user does not exist'],404);
+        $token = $user->first()->createToken($request->userAgent."_token");
+        return [
+            "user" => $user,
+            "token" => $token->plainTextToken
+        ];
+    }
+    public function register(Request $request){
+        $user = $request->validate([
+            "name" => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        if(User::where("email", $user['email'])->exists()){
             return response("Email already taken", 400);
         }
-        $user = User::factory()->create(["name"=>$json_body["name"],"email"=>$json_body["email"],"password"=>Hash::make($json_body["password"])]);
-        return response("ok");
+        $user['password'] = Hash::make($user['password']);
+        $user = User::factory()->create($user);
         $token = $request->user()->createToken($request->name);
         return ["token" => $token->plainTextToken];
     }
+
 }
--- buildit-pc-backend/database/migrations/2024_10_15_150130_create_user_rates_table.php
+++ buildit-pc-backend/database/migrations/2024_10_15_150130_create_user_rates_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\UserBuild;
+
 return new class extends Migration
 {
     /**
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_posts', function (Blueprint $table) {
+        Schema::create('user_rates', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_posts');
+        Schema::dropIfExists('user_rates');
     }
 };
