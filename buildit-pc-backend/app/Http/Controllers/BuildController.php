1e3300fc9827f57ce4396e13e5725d69591ea975
misterPython3 <genesismatic03@gmail.com>
2024-10-16 10:59:47 +0800

    added models, updated controllers, new migrations

--- buildit-pc-backend/app/Http/Controllers/BuildController.php
+++ buildit-pc-backend/app/Http/Controllers/BuildController.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\UserBuild;
 use Illuminate\Http\Request;
 use App\Models\CPU;
+use App\Models\User;
 
 class BuildController extends Controller
 {
     public function read(int $id){
 
-        return response("");
     }
-    
+    public function create(Request $request){
+
+    }
+    public function update(Request $request){
+
+    }
+    public function delete(int $id){
+
+    }
+    public function setPublish(Request $request){
+        $user = $request->user();
+        $build = UserBuild::where("user_id", $user->id)->where("build_hash",$request->build_hash);
+        if($build->exists()){
+
+        }
+    }
 }
--- buildit-pc-backend/app/Http/Controllers/TokenController.php
+++ buildit-pc-backend/app/Http/Controllers/TokenController.php
@@ -35,7 +35,8 @@ class TokenController extends Controller
     }
 
     public function register(Request $request){
-        if(User::where('email',$request->email)->exists())return ['message' => 'user already exist.'];
+        if(User::where('email',$request->email)->exists())
+            return ['message' => 'user already exist.'];
         $json_body = $request->json()->all();
         $user = User::factory()->create([
             'name' => $json_body['name'],
