edf762bcbce24d40bc4696059b56ffc9901c904d
misterRaccoon0 <geca.matic.up@phinmaed.com>
2024-10-20 20:25:37 +0800

    changes

--- buildit-pc-backend/database/migrations/2024_09_28_000745_create_user_builds_table.php
+++ buildit-pc-backend/database/migrations/2024_09_28_000745_create_user_builds_table.php
@@ -20,64 +20,14 @@ return new class extends Migration
     {
         Schema::create('user_build', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(CPU::class, 'cpu_id')
-                  ->nullable()
-                  ->constrained(
-                      app(CPU::class)->getTable(),
-                      'id'
-                  )
-                  ->cascadeOnUpdate()
-                  ->nullOnDelete();
-            $table->foreignIdFor(GPU::class,"gpu_id")
-                  ->nullable()
-                  ->constrained(
-                      app(GPU::class)->getTable(),
-                      'id'
-                  )
-                  ->cascadeOnUpdate()
-                  ->nullOnDelete();
-            $table->foreignIdFor(Motherboard::class,"motherboard_id")
-                  ->nullable()
-                  ->constrained(
-                      app(Motherboard::class)->getTable(),
-                      'id'
-                  )
-                  ->cascadeOnUpdate()
-                  ->nullOnDelete();
-            $table->foreignIdFor(RAM::class,"ram_id")
-                  ->nullable()
-                  ->constrained(
-                      app(RAM::class)->getTable(),
-                      'id'
-                  )
-                  ->cascadeOnUpdate()
-                  ->nullOnDelete();
-            $table->foreignIdFor(Storage::class,"storage_id")
-                  ->nullable()
-                  ->constrained(
-                      app(Storage::class)->getTable(),
-                      'id'
-                  )
-                  ->cascadeOnUpdate()
-                  ->nullOnDelete();
-            $table->foreignIdFor(PSU::class,"psu_id")
-                  ->constrained(
-                      app(PSU::class)->getTable(),
-                      'id'
-                  )
-                  ->cascadeOnUpdate()
-                  ->nullOnDelete();
-            $table->foreignIdFor(User::class,"user_id")
-                  ->constrained(
-                      app(User::class)->getTable(),
-                      'id'
-                  )
-                  ->nullOnDelete();
             $table->string('build_hash')->unique();
             $table->boolean('isPublic')->default(false);
         });
-    }
+        Schema::table('user_build',function($table){
 
+            $table->foreignId('user_id')->nullable()->constrained('users');
+        });
+    }
     /**
      * Reverse the migrations.
      */
