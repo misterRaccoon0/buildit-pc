559c89c8af3cc6fe7a969b96934cdfd958ff9959
misterPython3 <genesismatic03@gmail.com>
2024-10-21 19:49:16 +0800

    dev merge

--- buildit-pc-backend/routes/api.php
+++ buildit-pc-backend/routes/api.php
@@ -41,49 +41,56 @@ Route::prefix('/user')->group(function(){
 });
 
 
-// guarded route
-Route::middleware('auth:sanctum')->group(function(){
-    // route /token/*
-    Route::prefix('/token')->group(function(){
-        Route::get('/check',[TokenController::class, 'authenticate']);
-        Route::post('/login',[TokenController::class, 'login']);
-        Route::post('/logout',[TokenController::class, 'logout']);
-        Route::get('/refresh',[TokenController::class, 'refresh']);
-    });
-
-    // Route /build/*
-    Route::prefix('/build')->group(function(){
-        Route::get("/{build_hash}", [BuildController::class,"get"]);
-        Route::post('/', [BuildController::class, 'create']);
-        Route::put('/',[BuildController::class, 'update']);
-        Route::put('/publish',[BuildController::class,'setPublish']);
-        Route::delete('/',[BuildController::class, 'destroy']);
-
-    });
-
-});
-// route /user/*
-Route::prefix('/user')->group(function(){
-    Route::post('/register',[UserController::class]);
-    Route::post('/login',[UserController::class, 'login']);
-});
-
-
 // route /post/*
 Route::prefix('/post')->group(function(){
     Route::get('/',[UserPostController::class, 'get']);
-    Route::post('/comment',[UserPostController::class, 'comment']);
-    Route::get('/comment', [UserPostController::class, 'getComment']);
     Route::post('/rate', [UserPostController::class, 'rate']);
 
+    // route /post/comment/*
+    Route::prefix('/comment')->group(function(){
+
+        Route::get('/', [CommentController::class, 'getComment']);
+        // guarded route
+        Route::middleware('auth:sanctum')->group(function(){
+            Route::post('/',[CommentController::class, 'comment']);
+            Route::put('/',[CommentController::class, 'update']);
+            Route::delete('/',[CommentController::class, 'destroy']);
+        });
+    });
+});
+
+
+
+// route /component/*
+Route::prefix('/component')->group(function(){
+    Route::get(
+        "/{component_name}/{id}",
+        [ComponentController::class, "read"]
+    );
+    Route::get('/motherboard/support', []);
 });
 
 // route /admin/*
 Route::prefix('/admin')->group(function(){
-
-    Route::get("/component/{component_name}/{id}", [ComponentController::class, "read"]);
-    Route::post("/component/{component_name}", [ComponentController::class,"store"]);
-    Route::put("/component/{component_name}/{id}", [ComponentController::class, "update"]);
-    Route::delete("/component/{component_name}/{id}",[ComponentController::class,"delete"]);
+    // route /admin/component/*
+    Route::prefix('/component')->group(function(){
+        Route::post(
+            "/{component_name}",
+            [ComponentController::class,"store"]
+        );
+        Route::put(
+            "/{component_name}/{id}",
+            [ComponentController::class, "update"]
+        );
+        Route::delete(
+            "/{component_name}/{id}",
+            [ComponentController::class,"delete"]
+        );
+    });
 });
+
+
+
+
+
 
